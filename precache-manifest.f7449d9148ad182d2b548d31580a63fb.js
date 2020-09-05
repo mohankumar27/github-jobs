c72917bc4c786b101e1869e6d19acbdb810d4d57
mohankumar27 <mohankumar27197@gmail.com>
2020-09-05 18:49:55 +0530

    Updates

---
+++ precache-manifest.f7449d9148ad182d2b548d31580a63fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f3874081593f5d203144b2ba301687",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "27e84ae0fb930c49c6a6",
+    "url": "/github-jobs/static/css/main.b95b7e87.chunk.css"
+  },
+  {
+    "revision": "1210fcbff28455c2e1d5",
+    "url": "/github-jobs/static/js/2.f48803c8.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.f48803c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e84ae0fb930c49c6a6",
+    "url": "/github-jobs/static/js/main.d85d5f0c.chunk.js"
+  },
+  {
+    "revision": "e8f8862f689bdacc5a41",
+    "url": "/github-jobs/static/js/runtime-main.92b0d155.js"
+  }
+]);
